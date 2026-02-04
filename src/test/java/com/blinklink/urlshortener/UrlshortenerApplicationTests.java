3335aa10c3ae5ba14a810414091621a99e688488
imvmanish19 <imvmanish19@gmail.com>
2026-02-04 14:31:55 +0530

    Initialize Springboot project

---
+++ src/test/java/com/blinklink/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blinklink.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
